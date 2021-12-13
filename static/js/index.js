08ba2f15115ff5de91e278db69443297f2d561d8
“jamesquinn00git config --global user.email “jamesquinn00@outlook.com <jamesquinn00@outlook.comgit config --global user.name jamesquinn00>
2021-12-13 10:12:58 +0000

    Post request for login forms setup

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", loginListeners);
 
+const loginForm = document.querySelector("#login-form")
+const registerForm = document.querySelector("#register-form")
+
 // Event listeners for login page, to display/hide forms
 function loginListeners(){
-    loginBtn = document.querySelector("#login-btn")
-    registerBtn = document.querySelector("#register-btn")
+    openLoginForm = document.querySelector("#open-login")
+    registerBtn = document.querySelector("#open-register")
     backBtn = document.querySelector("#back-btn")
 
-    loginBtn.addEventListener("click", e=>{
-        showForm("#login-form")})
+    openLoginForm.addEventListener("click", e=>{
+        showForm(loginForm)})
     registerBtn.addEventListener("click", e=>{
-        showForm("#register-form")})
+        showForm(registerForm)})
 
     function showForm(form){
-        document.querySelector(form).style.display = "flex";
-        loginBtn.style.display = "none";
+        form.style.display = "flex";
+        openLoginForm.style.display = "none";
         registerBtn.style.display = "none";
         backBtn.style.display = "block";
     }
@@ -21,7 +24,7 @@ function loginListeners(){
     backBtn.addEventListener("click", e=>{
         document.querySelector("#register-form").style.display = "none";
         document.querySelector("#login-form").style.display = "none";
-        loginBtn.style.display = "block";
+        openLoginForm.style.display = "block";
         registerBtn.style.display = "block";
         backBtn.style.display = "none";
     })
@@ -31,6 +34,8 @@ function loginListeners(){
 // Confirm password when registering a user
 let password = document.querySelector("#register-password")
 let confirmPassword = document.querySelector("#confirm-password");
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
 
 function validatePassword(){
   if(password.value != confirmPassword.value) {
@@ -40,5 +45,35 @@ function validatePassword(){
   }
 }
 
-password.onchange = validatePassword;
-confirmPassword.onkeyup = validatePassword;
+// Handle login form data
+loginForm.addEventListener("submit", e=>{
+    e.preventDefault()
+    submitLogin(e, "login")
+})
+
+registerForm.addEventListener("submit", e=>{
+    e.preventDefault()
+    submitLogin(e, "register")
+})
+
+function submitLogin(e, path){
+    const loginData = {
+        email: e.target.email.value,
+        password: e.target.password.value
+    };
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(loginData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+    console.log(loginData)
+    // post to the '/blogs' URL
+    fetch(`http://localhost:3000/${path}`, options)
+        .then((r) => r.json())
+        .catch(console.warn);
+    // location.reload();
+}
+
